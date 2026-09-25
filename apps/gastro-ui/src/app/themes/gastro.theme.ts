714b607944624eb74bc9fa07dbe0fc90013c147d
StefanReichel <stefan.reichel@topdev.net>
2026-09-25 10:26:50 +0200

    update gastro.theme

--- apps/gastro-ui/src/app/themes/gastro.theme.ts
+++ apps/gastro-ui/src/app/themes/gastro.theme.ts
@@ -1,4 +1,24 @@
 import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
 
-export const GastroTheme = definePreset(Aura, {});
+export const GastroTheme = definePreset(Aura, {
+  components: {
+    tabs: {
+      tabpanel: {
+        padding: '1.5rem',
+      },
+    },
+    button: {
+      root: {
+        primary: {
+          borderColor: 'transparent',
+          hoverBorderColor: 'transparent',
+          activeBorderColor: 'transparent',
+          background: 'var(--red-accent)',
+          hoverBackground: 'var(--red-accent)',
+          activeBackground: 'var(--red-accent)',
+        },
+      },
+    },
+  },
+});
